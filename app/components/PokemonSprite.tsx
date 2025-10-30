638a7d6d9e0f364f49169f0ef309e3b27ca57889
PeterWang <wang.t.peter@gmail.com>
2025-10-29 23:32:26 -0700

    Images are like kinda working woooo.

---
+++ app/components/PokemonSprite.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { Pokedex } from "../data/pokedex";
+import { PokemonAltSprite } from "../data/pokemonAltSprite";
+
+export function PokemonSprite({ pokemonName }: { pokemonName: string }) {
+  // Because we're doing things with static images, it is better to not use state here and just deal with the constants directly.
+  const pokedexNumber =
+    PokemonAltSprite[pokemonName] || Pokedex[pokemonName].num;
+  const width = 40;
+  const height = 30;
+  const left = (pokedexNumber % 12) * width;
+  const top = Math.floor(pokedexNumber / 12) * height;
+
+  return (
+    <span
+      style={{
+        display: "inline-block",
+        background: `
+          transparent 
+          url(/pokemonicons-sheet.png) 
+          no-repeat 
+          scroll 
+          -${left}px -${top}px
+        `,
+        width,
+        height,
+        overflow: "visible",
+      }}
+    />
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { Text } from "@radix-ui/themes";
 import Image from "next/image";
 import { useState } from "react";
+import { PokemonSprite } from "./components/PokemonSprite";
 
 export default function Home() {
   const [allGames, setAllGames] = useState<string[]>([
@@ -10,6 +12,7 @@ export default function Home() {
     <div>
       <main>
         <Text>Gang gang</Text>
+        <PokemonSprite pokemonName="budew" />
       </main>
     </div>
   );
