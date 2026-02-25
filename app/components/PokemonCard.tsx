68aaa4969d34fa5fadca8854ffa93d8a8a40dec9
PeterWang <wang.t.peter@gmail.com>
2026-02-24 23:02:16 -0800

    Getting pokemoncard ready for border colors.

--- app/components/PokemonCard.tsx
+++ app/components/PokemonCard.tsx
@@ -5,11 +5,13 @@ import { PokemonSprite } from "./PokemonSprite";
 import { Button, Card, Text } from "@radix-ui/themes";
 
 export function PokemonCard({
-  PokemonDetails,
+  pokemonDetails,
   onClick,
+  borderColor,
 }: {
-  PokemonDetails: { name: string; id: number; types: string[] };
+  pokemonDetails: { name: string; id: number; types: string[] };
   onClick: (pokemon: { name: string; id: number; types: string[] }) => void;
+  borderColor?: string;
 }) {
   function formatName(str: string) {
     return str
@@ -21,23 +23,23 @@ export function PokemonCard({
 
   return (
     <Card
-      key={PokemonDetails.name}
+      key={pokemonDetails.name}
       className="pokemon-card"
-      onClick={() => onClick(PokemonDetails)}
+      onClick={() => onClick(pokemonDetails)}
       style={{ position: "relative", overflow: "hidden" }}
     >
       {/* Gradient Orb Background */}
       <GradientOrb
-        type1={PokemonDetails.types[0]}
-        type2={PokemonDetails.types[1]}
+        type1={pokemonDetails.types[0]}
+        type2={pokemonDetails.types[1]}
       />
 
-      {PokemonDetails.name !== "" && (
+      {pokemonDetails.name !== "" && (
         <Text style={{ textAlign: "center", position: "relative", zIndex: 1 }}>
-          {formatName(PokemonDetails.name)}
+          {formatName(pokemonDetails.name)}
         </Text>
       )}
-      <PokemonSprite pokemonName={PokemonDetails.name} scale={2} />
+      <PokemonSprite pokemonName={pokemonDetails.name} scale={2} />
     </Card>
   );
 }
